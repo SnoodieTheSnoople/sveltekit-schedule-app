7681f50f952701f29303dac3b404012acc1ef5c9
Cleef Gulfan <cleefgulfan312@outlook.com>
2023-11-23 23:36:05 +0000

    Test commit with different route and files

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="homepage" content="Homepage" />
+</svelte:head>
+
+<div>
+    <h1>This is a homepage</h1>
+    <p>This homepage is a test.</p>
+</div>
